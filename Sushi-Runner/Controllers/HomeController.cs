117fca61c70bb65e6e14716195c3b234741f5806
Oles Pankiv <ojlesua@gmail.com>
2019-03-22 13:17:23 +0200

    Initiate project

---
+++ Sushi-Runner/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sushi_Runner.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
