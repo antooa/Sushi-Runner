71f2a651580c33326742be850aca6b23c17d2885
antooa <antooa@yandex.ru>
2019-03-29 16:00:25 +0200

    Add Data Models

---
+++ Sushi-Runner/Data/Models/MealGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sushi_Runner.Models
+{
+    public class MealGroup
+    {
+        public string Name { get; set; }
+        public string Name { get; set; }
+    }
+}
