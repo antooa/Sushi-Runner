95d4aff53fc9a39f12fd02e9fc47130230ca556b
Oles Pankiv <ojlesua@gmail.com>
2019-04-12 14:42:01 +0300

    Add checks to mssql-docker.sh if container created

--- Sushi-Runner/Data/ApplicationDbContext.cs
+++ Sushi-Runner/Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SushiRunner.Models;
 
 namespace Sushi_Runner.Data
 {
-    public class ApplicationContext : DbContext
+    public class ApplicationContext : IdentityDbContext<User>
     {
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
--- mssql-docker.sh
+++ mssql-docker.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-docker run -d \
-    -e 'ACCEPT_EULA=Y' \
-    -e 'SA_PASSWORD=Pa$$word' \
-    -p 1433:1433 \
-    --name mssql \
-     mcr.microsoft.com/mssql/server:2017-latest
+IMAGE_NAME=mcr.microsoft.com/mssql/server:2017-latest
+CONTAINER_NAME=mssql
+CONTAINER_ID=$(docker container ls -qa --filter name=${CONTAINER_NAME})
+
+if [ -n "${CONTAINER_ID}" ]; then
+    docker container start ${CONTAINER_ID}
+else
+    docker run -d \
+        -e 'ACCEPT_EULA=Y' \
+        -e 'SA_PASSWORD=Pa$$word' \
+        -p 1433:1433 \
+        --name ${CONTAINER_NAME} \
+        ${IMAGE_NAME}
+fi
