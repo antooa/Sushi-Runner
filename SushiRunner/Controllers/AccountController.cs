3dd9b4d0b71442664e0c5b5a3132f02daf530d90
Oles Pankiv <ojlesua@gmail.com>
2019-05-01 00:01:15 +0300

    Add SignUp feature

--- SushiRunner/Controllers/AccountController.cs
+++ SushiRunner/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SushiRunner.Data.Entities;
+using SushiRunner.Services;
 using SushiRunner.ViewModels;
 
 namespace SushiRunner.Controllers
@@ -44,6 +46,75 @@ namespace SushiRunner.Controllers
             }
 
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User {Email = model.Email, UserName = model.Email};
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Action(
+                        "ConfirmEmail",
+                        "Account",
+                        new {userId = user.Id, code = code},
+                        protocol: HttpContext.Request.Scheme);
+                    EmailService emailService = new EmailService();
+                    await emailService.SendEmailAsync(model.Email, "Confirm your account",
+                        $"Confirm the registration by clicking on the link: <a href='{callbackUrl}'>confirmation link</a>");
+
+                    return Content(
+                        "To complete the registration, check the email and click on the link indicated in the mail.");
+                    // await _signInManager.SignInAsync(user, false);
+                    //return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return Redirect("/Home/Error");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return Redirect("/Home/Error");
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return Redirect("/Home/Error");
+            }
         }
     }
 }
