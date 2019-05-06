2168221e02800600b54198172aa9d5c0f8ab6f27
Valentin Khoroshchak <smartgtsua@gmail.com>
2019-05-07 01:45:28 +0300

    Refactored main page, added icons etc.

--- SushiRunner/wwwroot/js/home/index.js
+++ SushiRunner/wwwroot/js/home/index.js
@@ -2,3 +2,10 @@ $(".menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+var itemCount = 0;
+
+$('.add-to-card-btn').click(function (){
+    itemCount ++;
+    $('#itemCount').html(itemCount).css('display', 'flex');
+});
