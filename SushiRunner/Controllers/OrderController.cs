40b08763297a8db2c581b9e2fe60a099edd63331
Oles Pankiv <ojlesua@gmail.com>
2019-05-24 20:13:09 +0300

    he

--- SushiRunner/Controllers/OrderController.cs
+++ SushiRunner/Controllers/OrderController.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using ReturnTrue.AspNetCore.Identity.Anonymous;
+using SushiRunner.Data.Entities;
 using SushiRunner.Services.Dto;
 using SushiRunner.Services.Interfaces;
 using SushiRunner.ViewModels;
@@ -8,26 +14,44 @@ namespace SushiRunner.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IAccountService _accountService;
         private readonly IOrderService _orderService;
+        private readonly ICartService _cartService;
         private readonly IMapper _mapper;
-       
 
-        public OrderController(IOrderService orderService, IMapper mapper)
+
+        public OrderController(IAccountService accountService, IOrderService orderService,
+            ICartService cartService, IMapper mapper)
         {
+            _accountService = accountService;
             _orderService = orderService;
+            _cartService = cartService;
             _mapper = mapper;
         }
-        
-        [HttpPost]
-        public void Create([FromBody] OrderModel orderParams)
-        {
-            if (ModelState.IsValid)
-            {
-                var dto = _mapper.Map<OrderModel, OrderDTO>(orderParams);
-                _orderService.Create(dto);
-            }
 
-            RedirectToAction("Index", "Home");
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            var user = await _accountService.GetLoggedUserOrCreateAnonymous(
+                HttpContext.User,
+                HttpContext.Features.Get<IAnonymousIdFeature>()?.AnonymousId);
+            var cart = _cartService.GetByUserOrCreateNew(user);
+            var orderDto = new OrderDTO
+            {
+                CustomerName = "New User",
+                PhoneNumber = "+380981823859",
+                PaymentType = "CARD",
+                Address = "Lviv, Heroyiv UPA,  77",
+                PlacedAt = DateTime.Now,
+                Items = cart.Items.Select(item => new OrderItem
+                {
+                    Meal = item.Meal,
+                    Amount = item.Amount
+                }),
+                OrderStatus = OrderStatus.New
+            };
+            _orderService.Create(orderDto);
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpPut]
--- SushiRunner/wwwroot/js/home/cart.js
+++ SushiRunner/wwwroot/js/home/cart.js
@@ -1,9 +1,62 @@
-$(document).ready(function() {
-    $('#item-qty').on('click',
-        function() {
-            var qty = $(this).val();
-            var price = $('#item-price').html();
-            var itemSum = $('#item-sum');
-            itemSum.html(parseFloat(price).toFixed(2) * parseFloat(qty).toFixed(2) + '$');
-        })
+$(document).ready(function () {
+    $('.cart-amount-inc-button').on('click', incrementAmount);
 });
+
+$(document).ready(function () {
+    $('.cart-amount-dec-button').on('click', decrementAmount);
+});
+
+function incrementAmount() {
+    var amount = parseInt($(this).siblings('input').val());
+    if (amount) {
+        if (amount < 99) {
+            amount++;
+        }
+    } else {
+        amount = 1;
+    }
+    var price = $(this).closest('.cart-table-row')
+        .children('.price')
+        .text()
+        .trim()
+        .split(' ')[0];
+    price = parseInt(price);
+    var total = (price * amount).toString();
+    $(this).closest('.cart-table-row').children('.sum').text(total + 'грн.');
+    $(this).siblings('input').val(amount);
+    updateTotalPrice();
+}
+
+function decrementAmount() {
+    var amount = parseInt($(this).siblings('input').val());
+    if (amount) {
+        if (amount > 1) {
+            amount--;
+        }
+    } else {
+        amount = 1;
+    }
+    var price = $(this).closest('.cart-table-row')
+        .children('.price')
+        .text()
+        .trim()
+        .split(' ')[0];
+    price = parseInt(price);
+    var total = (price * amount).toString();
+    $(this).closest('.cart-table-row').children('.sum').text(total + 'грн.');
+    $(this).siblings('input').val(amount);
+    updateTotalPrice();
+}
+
+function updateTotalPrice() {
+    var total = 0;
+    $('.cart-table-row').each(function (index) {
+        var price = $(this).children('.sum')
+            .text()
+            .trim()
+            .split(' ')[0];
+        total += parseInt(price);
+    });
+
+    $('#total-sum').html("<strong>Загальна сума: </strong>" + total + " грн.");
+}
