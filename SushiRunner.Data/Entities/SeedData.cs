866d1078832d3044f15611026d9c08f782b01d8f
Oles Pankiv <ojlesua@gmail.com>
2019-05-24 15:38:28 +0300

    Add pizza meals

--- SushiRunner.Data/Entities/SeedData.cs
+++ SushiRunner.Data/Entities/SeedData.cs
@@ -64,8 +64,96 @@ namespace SushiRunner.Data.Entities
 
                 context.MealGroups.AddRange(groups);
 
-                
-                
+                context.Meals.AddRange(
+                    new Meal
+                    {
+                        ImagePath = "20190408_104639_апетитна.jpg",
+                        Name = "Піца Апетитна",
+                        Description =
+                            "Неополітанський соус, Сир моцарелла, Базилік, Перець болгарський, Бекон, Салямі гостре        ",
+                        Weight = 370,
+                        Price = 155,
+                        MealGroup = groups[0]
+                    },
+                    new Meal
+                    {
+                        ImagePath = "20190523_091736_капричоза.jpg",
+                        Name = "Акційна Піца Капричоза",
+                        Description = "Неополітанський соус, Сир моцарелла, Шинка, Печериці свіжі, Маслини        ",
+                        Weight = 390,
+                        Price = 72,
+                        MealGroup = groups[0]
+                    },
+                    new Meal
+                    {
+                        ImagePath = "20190408_104601_ББК.jpg",
+                        Name = "Піца BBQ",
+                        Description =
+                            "Неополітанський соус, Сир моцарелла, Салямі Чорізо, Синя цибуля, Огірки мариновані, Соус Барбекю        ",
+                        Weight = 450,
+                        Price = 115,
+                        MealGroup = groups[0]
+                    },
+                    new Meal
+                    {
+                        ImagePath = "20190408_105147_інферно.jpg",
+                        Name = "Піца Інферно",
+                        Description =
+                            "Неополітанський соус, Сир моцарелла, Бекон, Перець чілі, Мисливські ковбаски, Салямі Чорізо        ",
+                        Weight = 410,
+                        Price = 125,
+                        MealGroup = groups[0]
+                    },
+                    new Meal
+                    {
+                        ImagePath = "20190312_125051_5.jpg",
+                        Name = "Піца Бургер",
+                        Description =
+                            "Неополітанський соус, Сир моцарелла, Перець болгарський, Бекон, Синя цибуля, Курка смажена, Соус Бургер, Айсберг        ",
+                        Weight = 450,
+                        Price = 140,
+                        MealGroup = groups[0]
+                    },
+                    new Meal
+                    {
+                        ImagePath = "20190408_104737_Галицька.jpg",
+                        Name = "Піца Галицька",
+                        Description =
+                            "Неополітанський соус, Сир моцарелла, Печериці свіжі, Синя цибуля, Фарш яловичий, Часникова олія        ",
+                        Weight = 400,
+                        Price = 105,
+                        MealGroup = groups[0]
+                    },
+                    new Meal
+                    {
+                        ImagePath = "20190408_104806_Грибна.jpg",
+                        Name = "Піца Грибна",
+                        Description =
+                            "Сир моцарелла, Печериці свіжі, Вершковий соус, Сир пармезан, Опеньки консервовані, Шиітакі, Унагі        ",
+                        Weight = 410,
+                        Price = 145,
+                        MealGroup = groups[0]
+                    },
+                    new Meal
+                    {
+                        ImagePath = "20190408_104915_З білими грибами.jpg",
+                        Name = "Піца з Білими грибами",
+                        Description = "Сир моцарелла, Печериці свіжі, Вершковий соус, Сир пармезан, Білі гриби        ",
+                        Weight = 410,
+                        Price = 175,
+                        MealGroup = groups[0]
+                    },
+                    new Meal
+                    {
+                        ImagePath = "20190408_105107_Піца з тунцем і руколою.jpg",
+                        Name = "Піца з Тунцем та Руколою",
+                        Description =
+                            "Неополітанський соус, Сир моцарелла, Помідори чері, Рукола, Синя цибуля, Тунець консервований        ",
+                        Weight = 430,
+                        Price = 190,
+                        MealGroup = groups[0]
+                    }
+                );
 
 
                 context.SaveChanges();
